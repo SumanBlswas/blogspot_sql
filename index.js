620d991172573ad82c1aa19a304e8e99519980d5
Suman Biswas <sumanmanner@gmail.com>
2023-08-19 20:51:34 +0530

    updated

--- index.js
+++ index.js
@@ -13,28 +13,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   try {
     res.status(200).send({
-      EndPoints: [
-        {
-          "For GET":
-            "http://ec2-16-171-140-201.eu-north-1.compute.amazonaws.com/blogs",
-        },
-        {
-          "For GET with ID":
-            "http://ec2-16-171-140-201.eu-north-1.compute.amazonaws.com/blogs/:id",
-        },
-        {
-          "For POST":
-            "http://ec2-16-171-140-201.eu-north-1.compute.amazonaws.com/blogs",
-        },
-        {
-          "For PUT":
-            "http://ec2-16-171-140-201.eu-north-1.compute.amazonaws.com/blogs/:id",
-        },
-        {
-          "For DELETE with ID":
-            "http://ec2-16-171-140-201.eu-north-1.compute.amazonaws.com/blogs/:id",
-        },
-      ],
+      Hello: "Hello Api",
     });
   } catch (error) {
     res.status(400).send(error.message);
